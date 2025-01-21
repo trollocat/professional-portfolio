e522e21d702fa10bc22abe31fb80f2f4003facb9
Joan Ignacio Gonzalez <trollocat@gmail.com>
2025-01-21 12:19:37 -0300

    add prototype home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
+import { ThemeProvider } from "./ThemeProvider";
+import Header from "./components/Header";
 import Home from './pages/Home';
+import './App.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+        <footer>
+          <span>&copy; 2025 Joan Ignacio Gonzalez Portfolio</span>
+        </footer>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="hero-wrapper" >
+      <div className="hero-main hero-card">
+        <img src="portrait_transparent.webp" alt="portrait" />
+        <div className='dummy-spacing'></div>
+        <p>
+          My name is<strong> Joan Gonzalez</strong>, I’m a<strong> graphic designer </strong>and<strong> frontend developer</strong>
+        </p>
+      </div>
+      <div className="hero-secondary hero-card">My projects</div>
+      <div className="hero-secondary hero-card">My most impactful designs</div>
+
+    </div>
   )
 }
