9cb7145afead3331650686dda17a332d6577658a
Joan Ignacio Gonzalez <trollocat@gmail.com>
2025-02-13 04:19:07 -0300

    fix i18n interface

--- locales_resource_generator.js
+++ locales_resource_generator.js
@@ -1,22 +1,30 @@
-import { tocForResources } from 'i18next-resources-for-ts'
+import { tocForResources } from "i18next-resources-for-ts";
 
 const translation = {
-  name: 'translation',
-  path: './public/locales/en/translation.json'
-}
+  name: "translation",
+  path: "./public/locales/en/translation.json",
+};
 
 const home = {
-  name: 'home',
-  path: './public/locales/en/home.json'
-}
+  name: "home",
+  path: "./public/locales/en/home.json",
+};
 const projects = {
-  name: 'projects',
-  path: './public/locales/en/projects.json'
-}
+  name: "projects",
+  path: "./public/locales/en/projects.json",
+};
 
 const gallery = {
-  name: 'gallery',
-  path: './public/locales/en/gallery.json'
-}
+  name: "gallery",
+  path: "./public/locales/en/gallery.json",
+};
 
-const toc = tocForResources([translation, home, projects, gallery], './src/@types/resources.d.ts')
+const contact = {
+  name: "contact",
+  path: "./public/locales/en/contact.json",
+};
+
+const toc = tocForResources(
+  [translation, home, projects, gallery],
+  "./src/@types/resources.d.ts"
+);
